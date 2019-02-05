98fd3aff4db6ee2caaf4887230e454cbf664b8d4
Krzyrok <krzyrok@gmail.com>
2019-02-05 11:59:04 +0100

    add more entries in ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,5 +1,7 @@
 /* eslint-env node */
 module.exports = {
+  useYarn: true,
+
   scenarios: [
     {
       name: 'ember-lts-2.4',
@@ -78,6 +80,45 @@ module.exports = {
       npm: {
         devDependencies: {
           'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-cli': '2.12.3',
+          'ember-data': '2.18.5',
+          'ember-load-initializers': '1.1.0',
+          'ember-source': '2.12.2',
+          'ember-qunit': '3.5.3'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-cli': '2.16.2',
+          'ember-source': '2.16.4'
+        }
+      }
+    },
+    {
+      name: 'ember-3.1',
+      npm: {
+        devDependencies: {
+          'ember-cli': '3.1.4',
+          'ember-source': '3.1.2'
+        }
+      }
+    },
+    {
+      name: 'ember-3.7',
+      npm: {
+        devDependencies: {
+          'ember-cli': '3.7.1',
+          'ember-source': '3.7.2'
         }
       }
     },
